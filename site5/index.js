9787093f0e51b736a95e864e00c57eb3329ac45f
Костя <lkonstblack2000@mail.ru>
2022-12-14 17:01:08 +0700

    *

---
+++ site5/index.js
@@ -0,0 +1,16 @@
+
+
+function answers1() {
+
+document.getElementById('theRightAnswer').innerHTML = "Я тут вообще не удивлён";
+
+}
+
+function answers2() {
+
+
+document.getElementById('theRightAnswer').innerHTML = "Ты, на самом деле, пидор. Спасибо за внимание";
+
+
+
+}
